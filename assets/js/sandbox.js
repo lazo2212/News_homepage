be4cb3d22e01c647034843e679d053c16a9cf4e7
Robert Lazić <robert.lazicpz@gmail.com>
2023-02-26 14:18:08 +0100

    deleted:    .gitignore 	deleted:    README-template.md 	modified:   README.md 	modified:   assets/css/main.css 	modified:   assets/js/sandbox.js 	modified:   assets/sass/layout/_footer.scss 	modified:   assets/sass/layout/_header.scss 	modified:   assets/sass/main.scss 	deleted:    design/active-states.jpg 	deleted:    design/desktop-design.jpg 	deleted:    design/desktop-preview.jpg 	deleted:    design/mobile-design.jpg 	deleted:    design/mobile-menu.jpg 	modified:   index.html 	deleted:    style-guide.md

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,5 +1,13 @@
 const buttons = document.querySelectorAll('.menu');
 const sidebar = document.querySelector('.sidebar');
+const mainImg = document.querySelector('#main-img');
+
+// change mainImg based on window width
+if (window.innerWidth > 750) {
+  mainImg.src = '/assets/images/image-web-3-desktop.jpg';
+} else {
+  mainImg.src = '/assets/images/image-web-3-mobile.jpg';
+}
 
 buttons.forEach((button) =>
   button.addEventListener('click', () => {
